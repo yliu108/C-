aefd5b9fa6d8ed0af962f4115eaed66886b2bfa0
yliu108 <43041131+yliu108@users.noreply.github.com>
2020-09-12 10:16:22 -0600

    Code test

---
+++ CF/CF/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace CF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.UI_tbxSearch = new System.Windows.Forms.TextBox();
+            this.UI_cbxIsPhrase = new System.Windows.Forms.CheckBox();
+            this.UI_btnAddWord = new System.Windows.Forms.Button();
+            this.UI_btnSearch = new System.Windows.Forms.Button();
+            this.UI_btnClear = new System.Windows.Forms.Button();
+            this.UI_nudTop = new System.Windows.Forms.NumericUpDown();
+            this.UI_btnFindTop = new System.Windows.Forms.Button();
+            this.UI_cbxConjunction = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_nudTop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ChooseFile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UI_tbxSearch
+            // 
+            this.UI_tbxSearch.Location = new System.Drawing.Point(82, 186);
+            this.UI_tbxSearch.Name = "UI_tbxSearch";
+            this.UI_tbxSearch.Size = new System.Drawing.Size(100, 20);
+            this.UI_tbxSearch.TabIndex = 1;
+            // 
+            // UI_cbxIsPhrase
+            // 
+            this.UI_cbxIsPhrase.AutoSize = true;
+            this.UI_cbxIsPhrase.Location = new System.Drawing.Point(234, 188);
+            this.UI_cbxIsPhrase.Name = "UI_cbxIsPhrase";
+            this.UI_cbxIsPhrase.Size = new System.Drawing.Size(59, 17);
+            this.UI_cbxIsPhrase.TabIndex = 2;
+            this.UI_cbxIsPhrase.Text = "Phrase";
+            this.UI_cbxIsPhrase.UseVisualStyleBackColor = true;
+            // 
+            // UI_btnAddWord
+            // 
+            this.UI_btnAddWord.Location = new System.Drawing.Point(377, 182);
+            this.UI_btnAddWord.Name = "UI_btnAddWord";
+            this.UI_btnAddWord.Size = new System.Drawing.Size(75, 23);
+            this.UI_btnAddWord.TabIndex = 3;
+            this.UI_btnAddWord.Text = "Add";
+            this.UI_btnAddWord.UseVisualStyleBackColor = true;
+            this.UI_btnAddWord.Click += new System.EventHandler(this.UI_btnAddWord_Click);
+            // 
+            // UI_btnSearch
+            // 
+            this.UI_btnSearch.Location = new System.Drawing.Point(82, 232);
+            this.UI_btnSearch.Name = "UI_btnSearch";
+            this.UI_btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.UI_btnSearch.TabIndex = 4;
+            this.UI_btnSearch.Text = "Search";
+            this.UI_btnSearch.UseVisualStyleBackColor = true;
+            this.UI_btnSearch.Click += new System.EventHandler(this.UI_btnSearch_Click);
+            // 
+            // UI_btnClear
+            // 
+            this.UI_btnClear.Location = new System.Drawing.Point(234, 232);
+            this.UI_btnClear.Name = "UI_btnClear";
+            this.UI_btnClear.Size = new System.Drawing.Size(75, 23);
+            this.UI_btnClear.TabIndex = 5;
+            this.UI_btnClear.Text = "Clear";
+            this.UI_btnClear.UseVisualStyleBackColor = true;
+            this.UI_btnClear.Click += new System.EventHandler(this.UI_btnClear_Click);
+            // 
+            // UI_nudTop
+            // 
+            this.UI_nudTop.Location = new System.Drawing.Point(82, 281);
+            this.UI_nudTop.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.UI_nudTop.Name = "UI_nudTop";
+            this.UI_nudTop.Size = new System.Drawing.Size(120, 20);
+            this.UI_nudTop.TabIndex = 6;
+            this.UI_nudTop.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // UI_btnFindTop
+            // 
+            this.UI_btnFindTop.Location = new System.Drawing.Point(253, 281);
+            this.UI_btnFindTop.Name = "UI_btnFindTop";
+            this.UI_btnFindTop.Size = new System.Drawing.Size(75, 23);
+            this.UI_btnFindTop.TabIndex = 7;
+            this.UI_btnFindTop.Text = "FindTop";
+            this.UI_btnFindTop.UseVisualStyleBackColor = true;
+            this.UI_btnFindTop.Click += new System.EventHandler(this.UI_btnFindTop_Click);
+            // 
+            // UI_cbxConjunction
+            // 
+            this.UI_cbxConjunction.AutoSize = true;
+            this.UI_cbxConjunction.Location = new System.Drawing.Point(403, 283);
+            this.UI_cbxConjunction.Name = "UI_cbxConjunction";
+            this.UI_cbxConjunction.Size = new System.Drawing.Size(86, 17);
+            this.UI_cbxConjunction.TabIndex = 8;
+            this.UI_cbxConjunction.Text = "conjunctions";
+            this.UI_cbxConjunction.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UI_cbxConjunction);
+            this.Controls.Add(this.UI_btnFindTop);
+            this.Controls.Add(this.UI_nudTop);
+            this.Controls.Add(this.UI_btnClear);
+            this.Controls.Add(this.UI_btnSearch);
+            this.Controls.Add(this.UI_btnAddWord);
+            this.Controls.Add(this.UI_cbxIsPhrase);
+            this.Controls.Add(this.UI_tbxSearch);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.UI_nudTop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox UI_tbxSearch;
+        private System.Windows.Forms.CheckBox UI_cbxIsPhrase;
+        private System.Windows.Forms.Button UI_btnAddWord;
+        private System.Windows.Forms.Button UI_btnSearch;
+        private System.Windows.Forms.Button UI_btnClear;
+        private System.Windows.Forms.NumericUpDown UI_nudTop;
+        private System.Windows.Forms.Button UI_btnFindTop;
+        private System.Windows.Forms.CheckBox UI_cbxConjunction;
+    }
+}
+
---
+++ CF/CF/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime;
+namespace CF
+{
+    public partial class Form1 : Form
+    {
+        string temp;
+        List<KeyValuePair<String, bool>> Search=new List<KeyValuePair<string, bool>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            var slist = new List<string>();
+            var ofd = new OpenFileDialog();
+            var file = "";
+            if(ofd.ShowDialog()==DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(ofd.FileName);
+                    file = sr.ReadToEnd();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+                //var
+                temp = file;
+               
+               
+               
+                char[] a = file.ToCharArray();
+                var b = a.ToList();
+                b = b.Where(x => (x >= 64&&x<=90) ||(x>=97&&x<=122)).ToList();
+                var c = a.Where(x => char.IsPunctuation(x));
+                b.Sort();
+                Dictionary<char, int> Dic = new Dictionary<char, int>();
+                Dictionary<char, int> Dic2 = new Dictionary<char, int>();
+                Dictionary<string, int> Dic3 = new Dictionary<string, int>();
+                var a3 = temp.Split(' ', '\r', '\n').ToList();
+                var a3T = new List<string>(a3);
+                foreach (string x in a3)
+                {
+                    if (x.Any(y => !((y >= 64 && y <= 90) || (y >= 97 && y <= 122))) || x.Count() == 0)
+                        a3T.Remove(x);
+
+                }
+                foreach (string x in a3T)
+                {
+                    if (!Dic3.Keys.Contains(x))
+                        Dic3.Add(x, 1);
+                    else
+                        Dic3[x] += 1;
+
+                }
+                List<KeyValuePair<string, int>> List3 = Dic3.ToList();
+                List3.Sort((x, y) => x.Key.CompareTo(y.Key));
+                for (int i = 0; i < List3.Count; ++i)
+                {
+                        Console.WriteLine(List3[i]);
+                }
+                
+
+
+                foreach (char x in b)
+                {
+                    if (!Dic.Keys.Contains(x))
+                        Dic.Add(x, 1);
+                    else
+                        Dic[x] += 1;
+                }
+                foreach(char x in c)
+                {
+                    if (!Dic2.Keys.Contains(x))
+                        Dic2.Add(x, 1);
+                    else
+                        Dic2[x] += 1;
+                }
+               
+               
+                
+                List<KeyValuePair<char, int>> List1 = Dic.ToList();
+                List<KeyValuePair<char, int>> List2 = Dic2.ToList();
+                List2.Sort((x, y) => x.Value.CompareTo(y.Value));
+                for(int i=0;i<List1.Count;++i)
+                {
+                    Console.Write(List1[i]+" ");
+                }
+                Console.WriteLine();
+                for (int i = 0; i < List2.Count; ++i)
+                {
+                    Console.Write(List2[i] + " ");
+                }
+                Console.WriteLine();
+
+
+            }
+          
+
+        }
+        public List<KeyValuePair<String, int>> SearchByWord(List<KeyValuePair<String, bool>> Search, List<string> List)
+        {
+            List<KeyValuePair<string, int>> ReturnL = new List<KeyValuePair<string, int>>();
+            foreach (KeyValuePair<string, bool> a in Search)
+            {
+                if (a.Value == false)
+                {
+                    List<string> l = a.Key.Split(' ').ToList();
+                    for (int i = 0; i < List.Count; ++i)
+                    {
+                        if (l.All(x => List[i].Contains(x)))
+                            ReturnL.Add(new KeyValuePair<string, int>(List[i], i + 1));
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < List.Count; ++i)
+                    {
+                        if (List[i].Contains(a.Key))
+                            ReturnL.Add(new KeyValuePair<string, int>(List[i], i + 1));
+                    }
+                }
+            }
+            return ReturnL;
+        }
+
+       
+
+        private void UI_btnSearch_Click(object sender, EventArgs e)
+        {
+            if(temp==null)
+            {
+                Text = "Please choose a file.";
+                return;
+            }
+            if(Search.Count==0)
+            {
+                Text = "Please add words you want to search for.";
+                return;
+            }
+            var a4 = temp.Split('\r', '\n').ToList();
+            a4 = a4.Where(x => x.Count() != 0).ToList();
+            List<KeyValuePair<String, int>> Matches = SearchByWord(Search, a4);
+            foreach(KeyValuePair<string,int> i in Matches)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        private void UI_btnClear_Click(object sender, EventArgs e)
+        {
+            if(Search.Count!=0)
+            {
+                Search.Clear();
+            }
+        }
+
+        private void UI_btnFindTop_Click(object sender, EventArgs e)
+        {
+            if(temp==null)
+            {
+                return;
+            }
+            Dictionary<string, int> Dic3 = new Dictionary<string, int>();
+            var a3 = temp.Split(' ', '\r', '\n').ToList();
+            var a3T = new List<string>(a3);
+            foreach (string x in a3)
+            {
+                if (x.Any(y => !((y >= 64 && y <= 90) || (y >= 97 && y <= 122))) || x.Count() == 0)
+                    a3T.Remove(x);
+
+            }
+            foreach (string x in a3T)
+            {
+                if (!Dic3.Keys.Contains(x))
+                    Dic3.Add(x, 1);
+                else
+                    Dic3[x] += 1;
+
+            }
+            List<KeyValuePair<string, int>> List3 = Dic3.ToList();
+            List3.Sort((x, y) => y.Value.CompareTo(x.Value));
+            if (!UI_cbxConjunction.Checked)
+            {
+                for(int i=0;i<UI_nudTop.Value;++i)
+                {
+                    Console.WriteLine(List3[i]);
+                }
+            }
+            else
+            {
+                Dictionary<int, string> Dic4 = new Dictionary<int, string>();
+                foreach (KeyValuePair<string, int> x in Dic3)
+                {
+                    if (!Dic4.Keys.Contains(x.Value))
+                        Dic4.Add(x.Value, x.Key);
+                    else
+                        Dic4[x.Value] += ' ' + x.Key;
+
+                }
+                List<KeyValuePair<int, string>> List4 = Dic4.ToList();
+                List4.Sort((x, y) => y.Key.CompareTo(x.Key));
+                int top = List4.Count;
+                if (UI_nudTop.Value < top)
+                    top = (int)UI_nudTop.Value;
+                for (int i = 0; i < top; ++i)
+                {
+                    Console.WriteLine(List4[i]);
+                }
+            }
+        }
+
+        private void UI_btnAddWord_Click(object sender, EventArgs e)
+        {
+            if (UI_cbxIsPhrase.Checked)
+            {
+                Search.Add(new KeyValuePair<string, bool>(UI_tbxSearch.Text, true));
+            }
+            else
+            {
+                Search.Add(new KeyValuePair<string, bool>(UI_tbxSearch.Text, false));
+            }
+        }
+    }
+}
